02b95bcf36a94f34db1983e7953766cda28ac751
neirien <nehirelcim@gmail.com>
2022-05-04 20:21:57 +0300

    ye

--- mobil/Assets/Scripts/Generation.cs
+++ mobil/Assets/Scripts/Generation.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Generation : MonoBehaviour
 {
-
-    public GameObject Prefab;
+    List<GameObject> prefabList = new List<GameObject>();
+    public GameObject Prefab1;
+    public GameObject Prefab2;
+    public GameObject Prefab3;
     public GameObject spawnPoint;
+    
 
+    void Start()
+    {
+        prefabList.Add(Prefab1);
+        prefabList.Add(Prefab2);
+        prefabList.Add(Prefab3);
+        
+
+    }
 
     private void OnTriggerEnter(Collider Platform)
     {
-        Instantiate(Prefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+        int prefabIndex = UnityEngine.Random.Range(0, 3);
+        Instantiate(prefabList[prefabIndex], spawnPoint.transform.position, spawnPoint.transform.rotation);
     }
 }
